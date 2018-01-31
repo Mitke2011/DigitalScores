258980525a6790c1dc462606992459b99afe7b64
Stefan Mitić <mitkeprepecenica@gmail.com>
2018-01-31 18:33:02 +0100

    Add project files.

---
+++ DigitalScores/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DigitalScores.Startup))]
+namespace DigitalScores
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
