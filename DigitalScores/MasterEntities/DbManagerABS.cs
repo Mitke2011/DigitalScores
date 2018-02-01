46e464f1063c643c937b7b44fb3a3409cce0ecd3
Stefan Mitić <mitkeprepecenica@gmail.com>
2018-02-01 18:00:01 +0100

    - Resolving project building issues - Added Db managers

---
+++ DigitalScores/MasterEntities/DbManagerABS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigitalScores.MasterEntities
+{
+    public abstract class DbManagerABS
+    {
+        public abstract void Insert();
+        public abstract void DeleteSingle();
+        public abstract void DeleteRange();
+        public abstract List<object> GetAll();
+        public abstract object GetSingle(int id);
+        public abstract void Update(object carrier);
+    }
+}
--- DigitalScores/Startup.cs
+++ DigitalScores/Startup.cs
@@ -8,7 +8,7 @@ namespace DigitalScores
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            //ConfigureAuth(app);
         }
     }
 }
