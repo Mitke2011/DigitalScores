20d885f1b05647fc5dc4b6c81483ea5b4acd5be8
Stefan Mitić <mitkeprepecenica@gmail.com>
2018-03-06 22:16:14 +0100

    Revert "Task: Resolving server issues - Attempt #1"
    This reverts commit b243f364278981538414c3cd441abf3505b7c685.

--- DigitalScores/Controllers/KoloController.cs
+++ DigitalScores/Controllers/KoloController.cs
@@ -25,7 +25,7 @@ namespace DigitalScores.Controllers
         // GET: Kolo/Create
         public ActionResult Create()
         {
-            return View("KoloEntries");
+            return View("KoloEntry");
         }
 
         // POST: Kolo/Create
